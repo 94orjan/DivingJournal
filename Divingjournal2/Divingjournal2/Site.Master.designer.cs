2bdba7e8c243520fd72406d6c2f2d9a6c467fec6
Nils Terje <nilsi_9@hotmail.com>
2017-06-12 19:21:09 +0200

    12.06.17 19:20

--- Divingjournal2/Divingjournal2/Site.Master.designer.cs
+++ Divingjournal2/Divingjournal2/Site.Master.designer.cs
@@ -22,22 +22,13 @@ namespace Divingjournal2 {
         protected global::System.Web.UI.WebControls.Label Label_User;
         
         /// <summary>
-        /// Button1 control.
+        /// Button2 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
-        
-        /// <summary>
-        /// Button1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.Button Button2;
         
         /// <summary>
         /// MainContent control.
